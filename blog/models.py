3b183c97dc07827216cdbfe8b3897dee7cdb0cd9
fIenETICmn <simarpreet@codefront.io>
2020-12-29 13:10:27 +0530

    refactor:fix code

--- blog/models.py
+++ blog/models.py
@@ -93,6 +93,7 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     item = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
     price = models.BooleanField(default=False)
 
     def __str__(self):
